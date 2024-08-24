d546aa35f232580fa769f9383127869217186cda
dhanush11T <128609138+dhanush11T@users.noreply.github.com>
2024-08-24 10:51:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,29 +8,20 @@ const router = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Define allowed origins
-const allowedOrigins = [
-  'https://ecomm-goo-frontend.onrender.com',
-  'http://localhost:3000',
-  'https://66c964adff6a241c910f130b--jocular-dusk-339aa0.netlify.app',
-  'https://66c96738d921d37d917a7ecf--regal-twilight-d650f7.netlify.app',
-  // Add other origins as needed
-];
+// Middleware to set CORS headers manually
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://ecomm-goo-frontendd.onrender.com');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // Middleware
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        // Allow requests with no origin (e.g., mobile apps or curl requests)
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: 'https://ecomm-goo-frontendd.onrender.com',
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -49,9 +40,4 @@ connectDB()
     app.listen(PORT, () => {
       console.log("Connected to MongoDB");
       console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err.message);
-    process.exit(1); // Exit process with failure
-  });
+   ​⬤
