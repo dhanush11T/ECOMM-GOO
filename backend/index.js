fcf34b4e4dfd37a0825b90eaaa08ce82e8913199
dhanush11T <128609138+dhanush11T@users.noreply.github.com>
2024-08-24 12:18:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://ecomm-goo-frontendd.onrender.com/',
+  origin: 'ecomm-goo-1hbf.vercel.app/',
   credentials: true,
 }));
 app.use(express.json());
