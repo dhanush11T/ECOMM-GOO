79dd7e653d2ce4557ad4161b7203b671f53882ab
dhanush11T <128609138+dhanush11T@users.noreply.github.com>
2024-08-24 13:55:39 +0530

    Update authToken.js

--- backend/middleware/authToken.js
+++ backend/middleware/authToken.js
@@ -3,33 +3,35 @@ const jwt = require("jsonwebtoken");
 async function authToken(req, res, next) {
   try {
     const token = req.cookies?.token;
-    if(!token){
-        return res.status(200).json({
-            message : "Please Login...!",
-            error : true,
-            success : false
-        })
+    if (!token) {
+      return res.status(401).json({
+        message: "Please Login...!",
+        error: true,
+        success: false
+      });
     }
+
     console.log("token----", token);
+
     jwt.verify(token, process.env.TOKEN_SECRET_KEY, function(err, decoded) {
-        console.log(err)
-        console.log("decoded",decoded)
-        
-        if(err){
-            console.log("error auth", err)
-        }
+      if (err) {
+        console.log("error auth", err);
+        return res.status(403).json({
+          message: "Failed to authenticate token",
+          error: true,
+          success: false
+        });
+      }
 
-        req.userId = decoded?._id
-
-        next()
+      req.userId = decoded?._id;
+      next();
     });
     
   } catch (err) {
-    res.status(400).json({
+    res.status(500).json({
       message: err.message || err,
-      data: [],
       error: true,
-      success: false,
+      success: false
     });
   }
 }
