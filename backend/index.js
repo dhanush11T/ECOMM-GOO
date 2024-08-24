03070488a4d786213f566c53e86b51e73837a1ce
dhanush11T <128609138+dhanush11T@users.noreply.github.com>
2024-08-24 12:50:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,26 +6,10 @@ const connectDB = require("./config/db");
 const router = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
-
-// Middleware to set CORS headers manually
-const allowedOrigins = [
-    'https://ecomm-goo-1hbf.vercel.app',
-    'https://ecomm-goo-git-main-dhanush11ts-projects.vercel.app',
-    // Add any other domains if needed
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    origin: 'https://ecomm-goo-1hbf.vercel.app', // Frontend domain
+    credentials: true,
 }));
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -35,10 +19,14 @@ app.use("/api", router);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send(`Something went wrong! Error: ${err.message}`);
+    console.error(err.stack);
+    res.status(500).send(`Something went wrong! Error: ${err.message}`);
 });
 
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 // Connect to MongoDB and start server
 connectDB()
   .then(() => {
